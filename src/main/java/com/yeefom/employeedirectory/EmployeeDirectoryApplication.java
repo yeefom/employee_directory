8e961862c284c61b460f1e749b0e9143cfcd40ce
Yifeng Wang <yifengw@opentable.com>
2019-04-29 15:05:13 -0700

    set up project

---
+++ src/main/java/com/yeefom/employeedirectory/EmployeeDirectoryApplication.java
@@ -0,0 +1,13 @@
+package com.yeefom.employeedirectory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeDirectoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeDirectoryApplication.class, args);
+	}
+
+}
