fc028ed5c22a3237a44a7265dda6d9155d824d21
Yifeng Wang <yifengw@opentable.com>
2019-05-01 10:52:42 -0700

    handle put and delete

--- src/main/java/com/yeefom/employeedirectory/rest/EmployeeController.java
+++ src/main/java/com/yeefom/employeedirectory/rest/EmployeeController.java
@@ -6,9 +6,11 @@ import com.yeefom.employeedirectory.entity.Employee;
 import com.yeefom.employeedirectory.service.EmployeeService;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -39,6 +41,17 @@ public class EmployeeController {
         employee.setId(0); // force item to be saved as new even if id is provided
         employeeService.save(employee);
         return employee;
+    }
+
+    @PutMapping("/employees")
+    public Employee updateEmployee(@RequestBody Employee employee) {
+        employeeService.save(employee);
+        return employee;
+    }
+
+    @DeleteMapping("/employees/{id}")
+    public void deleteEmployee(@PathVariable int id) {
+        employeeService.delete(id);
     }
 
 }
