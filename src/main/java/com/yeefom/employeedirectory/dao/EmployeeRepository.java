df6b6e5fd7c12f9dc9f67c46b9a183e573d1d0d0
Yifeng Wang <yifengw@opentable.com>
2019-05-02 16:12:41 -0700

    expose endpoints through spring data rest

--- src/main/java/com/yeefom/employeedirectory/dao/EmployeeRepository.java
+++ src/main/java/com/yeefom/employeedirectory/dao/EmployeeRepository.java
@@ -3,7 +3,9 @@ package com.yeefom.employeedirectory.dao;
 import com.yeefom.employeedirectory.entity.Employee;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource(path="members") // "employees" by default
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 
 }
